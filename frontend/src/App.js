c59db70656cdf8695c9ad522ef95237c649e45ef
Colby <colby.wang765@gmail.com>
2023-11-03 02:04:09 -0400

    Testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,11 +10,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:8000/auth/google"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Login
         </a>
       </header>
     </div>
