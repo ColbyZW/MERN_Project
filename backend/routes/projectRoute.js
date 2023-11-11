265e67c289537df79f571bb6ad3a0bf3f0e3a963
Stephen An <stephen.an@gmail.com>
2023-11-10 22:58:42 -0500

    test

--- backend/routes/projectRoute.js
+++ backend/routes/projectRoute.js
@@ -236,7 +236,7 @@ projectRouter.get('/search', async (req, res) => {
         const searchRegex = new RegExp(searchTerm, 'i');
 
         const results = await Project.find({ description: searchRegex });
-        res.send(results);
+        res.json(results);
     } catch (error) {
         res.status(500).send(error.message);
     }
